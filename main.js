46599c14a0939b9c556dae5dec3b82a25a3e652b
Karim Shaloh <shal0054@algonquinlive.com>
2021-11-12 18:44:51 -0400

    all 3 methods implemented

--- main.js
+++ main.js
@@ -8,6 +8,22 @@ const data = require('./data.js');
 
 const characters = data.characters;
 
-characters.forEach((character) => {
-  console.log(character);
-});
+function printChars() {
+  characters.forEach((character) => {
+    console.log(character);
+  });
+
+  let chars = characters.filter((char) => {
+    return char.race[0] !== 'S';
+  });
+  console.log(chars);
+
+  chars.sort((a, b) => {
+    if (a.name > b.name) return 1;
+    else if (a.name < b.name) return -1;
+    return 0;
+  });
+  console.log(chars);
+}
+
+printChars();
